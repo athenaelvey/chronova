ab72ae40324168431d1610403ef53e55c882cfe7
Athena Elvey <athenaelvey@gmail.com>
2026-08-27 16:28:35 -0400

    Begun environment building for FastAPI backend and defined Pulsar SQLAlchemy mode

---
+++ backend/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Enum
+from database import Base
+
+class Pulsar(Base):
+    __tablename__ = "pulsars"
+    id = Column(Integer, primary_key=True)
+    PSRJ = Column(String, unique=True, nullable=False)
+    classification = Column(Enum("Magnetars", "Ordinary Pulsars","Millisecond Pulsars", name="classification_enum"))
+    TYPE = Column(String)
+    P0 = Column(Float, nullable=False)
+    P0_ERR = Column(Float)
+    P1 = Column(Float)
+    P1_ERR = Column(Float)
+    DIST = Column(Float)
+
+
